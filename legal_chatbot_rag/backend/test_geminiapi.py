c7c1f1720af58b27963bc5498af6b1a74a02510d
Sarvagna <sarvagna.nandyala@gmail.com>
2025-03-31 11:36:01 +0530

    Setup app.py for testing Gemini API key

--- legal_chatbot_rag/backend/test_geminiapi.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDKiZI2ikH8avxkBmHbI2SBi0YL7lfS5kk")
-
-models = genai.list_models()
-for model in models:
-    print(model.name, model.supported_generation_methods)
